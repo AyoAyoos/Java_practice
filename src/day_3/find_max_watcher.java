9f38e56a21568983b059f7c8fa446cea255c2868
AyoAyoos <nakodayush@gmail.com>
2026-06-03 15:47:36 +0530

    max_finder_using_

--- src/day_3/find_max_watcher.java
+++ src/day_3/find_max_watcher.java
@@ -9,4 +9,25 @@ package day_3;
 
 public class find_max_watcher {
 
-}
+	
+	 public static void main(String[]args) {
+		 
+		 
+
+		        int[] arr = {25,45,74,80,90,40,60,80,40,50,40};
+		        
+		        
+		        int maxSum = arr[0]+arr[1]+arr[2];
+		        
+		        for (int i=3; i<arr.length ; i++) {
+		        	int sum=arr[i]+arr[i-1]+arr[i-2];
+		        	if(sum > maxSum)
+		        		maxSum = sum;
+		 
+		        }
+
+		        System.out.println(maxSum);
+		    }
+	 
+	 }
+

e3f438dd935e45835c821caee2a6eeffd7401fb0
AyoAyoos <nakodayush@gmail.com>
2026-06-04 11:35:30 +0530

    alternate reverse question

--- src/day_4/LS_q4.java
+++ src/day_4/LS_q4.java
@@ -1,5 +1,41 @@
 package day_4;
 
-public class LS_q4 {
 
+//WAP reverse the alternative substring
+//I/P ;- india is my country
+//O/p :- india si my yrtnuoc
+
+
+public class LS_q4 {
+	
+	public static void main(String[] args) {
+
+        String str = "india is my country";
+
+        String[] words = str.split(" ");
+
+        for (int i = 1; i < words.length; i = i + 2) {
+
+            char[] arr = words[i].toCharArray();
+
+            int left = 0;
+            int right = arr.length - 1;
+
+            while (left < right) {
+
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+
+            words[i] = new String(arr);
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words[i] + " ");
+        }
+    }
 }

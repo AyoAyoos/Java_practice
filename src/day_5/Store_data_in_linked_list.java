e98922f007f556e6d9c92c965b4b65d3f9986e27
AyoAyoos <nakodayush@gmail.com>
2026-06-05 15:48:02 +0530

    linklist  example_2

--- src/day_5/Store_data_in_linked_list.java
+++ src/day_5/Store_data_in_linked_list.java
@@ -1,5 +1,39 @@
 package day_5;
 
-public class Store_data_in_linked_list {
 
-}
+
+//WAP to store any format of data in linked list with 2 node
+
+public class Store_data_in_linked_list {
+	
+	
+	    public static void main(String[] args) {
+
+	        // Node 1 stores String
+	        Node node1 = new Node("Ayush");
+
+	        // Node 2 stores Integer
+	        Node node2 = new Node(101);
+
+	        // Link nodes
+	        node1.next = node2;
+
+	        // Print data
+	        System.out.println("Node 1 Data: " + node1.data);
+	        System.out.println("Node 2 Data: " + node2.data);
+	    }
+	}
+
+	class Node {
+
+	    Object data;
+	    Node next;
+
+	    Node(Object data) {
+	        this.data = data;
+	        this.next = null;
+	    }
+	}
+	
+
+

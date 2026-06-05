ebf4c3cbe2ff8b3590c07313c69d9091657d2f75
AyoAyoos <nakodayush@gmail.com>
2026-06-05 11:33:05 +0530

    sum of even numbers

---
+++ src/day_5/Sum_of_numbers_using_recursion.java
@@ -0,0 +1,28 @@
+package day_5;
+
+//sum of all even numbers using recursion
+
+
+
+public class Sum_of_numbers_using_recursion {
+
+	
+
+	    public static void main(String[] args) {
+
+	    	Sum_of_numbers_using_recursion obj = new Sum_of_numbers_using_recursion();
+
+	        int result = obj.sumEven(50);
+
+	        System.out.println("Sum = " + result);
+	    }
+
+	    public int sumEven(int n) {
+
+	        if (n == 0) {
+	            return 0;
+	        }
+
+	        return n + sumEven(n - 2);
+	    }
+	}

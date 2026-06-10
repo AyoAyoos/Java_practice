cc550af1248b99bb053236b5718974cfa66b4377
AyoAyoos <nakodayush@gmail.com>
2026-06-10 10:19:32 +0530

    Directed graph

--- src/day_9/Directed_Graph.java
+++ src/day_9/Directed_Graph.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 
 public class Directed_Graph {
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		
 		int v =3 ;
 		
 		ArrayList<Integer>[] graph = new ArrayList[v];
 		
 		for (int i = 0; i < v ; i++) {
-			graph[i] = new ArrayList();
+			graph[i] = new ArrayList<>();
 		}
 		
+		graph[0].add(1);
+		graph[0].add(2);
+		graph[1].add(9);
+		graph[1].add(7);
+		
+		
+		for(int i =0 ; i< v ; i++) {
+			System.out.println( i + "->" + graph[i]);
+		}
 	}
 
 }

e903e3304fbabf573b05adaaecce1c4e8cd59eb7
AyoAyoos <nakodayush@gmail.com>
2026-06-10 10:11:15 +0530

    Directed_graph

---
+++ src/day_9/Directed_Graph.java
@@ -0,0 +1,19 @@
+package day_9;
+
+import java.util.ArrayList;
+
+public class Directed_Graph {
+	
+	public void main(String[] args) {
+		
+		int v =3 ;
+		
+		ArrayList<Integer>[] graph = new ArrayList[v];
+		
+		for (int i = 0; i < v ; i++) {
+			graph[i] = new ArrayList();
+		}
+		
+	}
+
+}

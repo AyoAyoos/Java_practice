2fc9ecc71a744b0a51d794d870162737f54c9403
AyoAyoos <nakodayush@gmail.com>
2026-06-10 10:27:19 +0530

    Questions_on_Directed_graph

---
+++ src/day_9/Create_direscted_graph.java
@@ -0,0 +1,18 @@
+package day_9;
+
+import java.util.ArrayList;
+
+public class Create_direscted_graph {
+	
+public static void main(String[] args) {
+		
+		int v =5 ;
+		
+		ArrayList<Integer>[] graph = new ArrayList[v];
+		
+		for (int i = 0; i < v ; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
+}
+}
--- src/day_9/Directed_Graph.java
+++ src/day_9/Directed_Graph.java
@@ -4,25 +4,32 @@ import java.util.ArrayList;
 
 public class Directed_Graph {
 	
-	public static void main(String[] args) {
-		
-		int v =3 ;
-		
-		ArrayList<Integer>[] graph = new ArrayList[v];
-		
-		for (int i = 0; i < v ; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		
-		graph[0].add(1);
-		graph[0].add(2);
-		graph[1].add(9);
-		graph[1].add(7);
-		
-		
-		for(int i =0 ; i< v ; i++) {
-			System.out.println( i + "->" + graph[i]);
+	/*
+	 * WAP to create the directed graph for below friend circle :
+	 * A->B->C
+	 * B->D
+	 * D->E
+	 */
+
+		public static void main(String[] args) {
+
+			int V = 5;
+
+			ArrayList<Integer>[] graph = new ArrayList[V];
+
+			for (int i = 0; i < V; i++) {
+				graph[i] = new ArrayList<>();
+			}
+
+			graph[0].add(1); // A -> B
+			graph[1].add(2); // B -> C
+			graph[1].add(3); // B -> D
+			graph[3].add(4); // D -> E
+
+			char[] name = {'A', 'B', 'C', 'D', 'E'};
+
+			System.out.println(name[0] + "->" + name[1] + "->" + name[2]);
+			System.out.println(name[1] + "->" + name[3]);
+			System.out.println(name[3] + "->" + name[4]);
 		}
 	}
-
-}
